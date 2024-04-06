81d0d113bac47971157645f7f1804b733f375884
Mateo Cakoni <cakoniteo@gmail.com>
2024-04-06 11:18:13 +0200

    plotsuam klasat

--- Models/UnavailabilityPeriods.cs
+++ Models/UnavailabilityPeriods.cs
@@ -2,5 +2,8 @@
 {
     public class UnavailabilityPeriods
     {
+        public int Id { get; private set; }
+        public DateTime StartDate { get; private set; }
+        public DateTime EndDate { get; private set; }
     }
 }
--- Models/Users.cs
+++ Models/Users.cs
@@ -2,5 +2,11 @@
 {
     public class Users
     {
+        public int Id { get; set; } 
+        public string Name { get; set; } 
+        public string Surname { get; set; } 
+        public string Email { get; set; } 
+        public bool IsDeleted { get; set; } 
+
     }
 }
